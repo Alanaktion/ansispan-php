688a4fe93d9602181fd01aa5bf4396abb49dd854
Alan Hardman <me@imalan.tk>
2013-02-11 20:31:53 -0700

    Final Release
    Minecraft logs use a different format, I'll start a new repo for
    MCLogParse again...

--- ansispan.inc.php
+++ ansispan.inc.php
@@ -1,42 +1,62 @@
 <?php
 
-// ansispan
 // Parses text with ANSI control codes and returns HTML
 function ansispan($str) {
 
-	// Foreground colors
+	// Colors
 	$fgColors = array(
-		'30' => 'black',
-		'31' => 'red',
-		'32' => 'green',
-		'33' => 'yellow',
-		'35' => 'purple',
-		'36' => 'cyan',
-		'37' => 'white'
+		30 => 'black',
+		31 => 'red',
+		32 => 'green',
+		33 => 'yellow',
+		34 => 'blue',
+		35 => 'magenta',
+		36 => 'cyan',
+		37 => 'white'
+	);
+	$bgColors = array(
+		40 => 'black',
+		41 => 'red',
+		42 => 'green',
+		43 => 'yellow',
+		44 => 'blue',
+		45 => 'magenta',
+		46 => 'cyan',
+		47 => 'white'
 	);
 
-	// Replace color codes
+	// Replace foreground color codes
 	foreach(array_keys($fgColors) as $color) {
-		$span = '<span style="color: ' + $fgColors[$color] + '">';
+		$span = '<span style="color: '.$fgColors[$color].'">';
 		
-		// \033[Xm == \033[0;Xm sets foreground color to X.
-		$str = preg_replace('/\033\\['.$color.'m/',$span,$str);
-		$str = preg_replace('/\033\\[0;'.$color.'m/',$span,$str);		
+		// 33[Xm == 33[0;Xm sets foreground color to X
+		$str = preg_replace("/\x1B33\[".$color.'m/',$span,$str);
+		$str = preg_replace("/\x1B33\[0;".$color.'m/',$span,$str);
 	}
 	
-	// \033[1m enables bold font, \033[22m disables it
-	$str = preg_replace('/\033\[1m/','<b>',$str);
-	$str = preg_replace('/\033\[22m/','</b>',$str);
+	// Replace background color codes
+	foreach(array_keys($fgColors) as $color) {
+		$span = '<span style="background-color: '.$fgColors[$color].'">';
+		
+		// 33[Xm == 33[0;Xm sets background color to X
+		$str = preg_replace("/\x1B33\[".$color.'m/',$span,$str);
+		$str = preg_replace("/\x1B33\[0;".$color.'m/',$span,$str);
+	}
+	
+	// 33[1m enables bold font, 33[22m disables it
+	$str = preg_replace("/\x1B33\[1m/",'<b>',$str);
+	$str = preg_replace("/\x1B33\[22m/",'</b>',$str);
 
-	// \033[3m enables italics font, \033[23m disables it
-	$str = preg_replace('/\033\[3m/','<i>',$str);
-	$str = preg_replace('/\033\[23m/','</i>',$str);
+	// 33[3m enables italics font, 33[23m disables it
+	$str = preg_replace("/\x1B33\[3m/",'<i>',$str);
+	$str = preg_replace("/\x1B33\[23m/",'</i>',$str);
 
 	// Catch any remaining close tags
-	$str = preg_replace('/\033\[m/','</span>',$str);
-	$str = preg_replace('/\033\[0m/','</span>',$str);
+	$str = preg_replace("/\x1B33\[m/",'</span>',$str);
+	$str = preg_replace("/\x1B33\[0m/",'</span>',$str);
 	
-	return preg_replace('/\033\[39m/', '</span>', $str);
+	// Replace "default" codes with closing span
+	return preg_replace("/\x1B33\[(39|49)m/",'</span>', $str);
 	
 }
 
--- test.php
+++ test.php
@@ -1,30 +1,26 @@
 <?php
 require 'ansispan.inc.php';
 
-$log = <<<EOT
-2013-02-02 03:47:25 [INFO] [[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> I got those skeletons good :D[m
-2013-02-02 03:47:45 [INFO] [[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> haha[m
-2013-02-02 03:48:23 [INFO] [[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> lol I have no idea what I'm doing :P[m
-2013-02-02 03:48:31 [INFO] [[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> hehe XD[m
-2013-02-02 03:51:37 [INFO] [[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> brb[m
-2013-02-02 03:58:05 [INFO] alanaktion issued server command: /afk
-2013-02-02 03:59:58 [INFO] Read timed out
-2013-02-02 03:59:58 [INFO] alanaktion lost connection: disconnect.endOfStream
-2013-02-02 04:01:14 [INFO] alanaktion [/74.81.242.196:60248] lost connection
-2013-02-02 04:01:14 [INFO] alanaktion[/74.81.242.196:60259] logged in with entity id 70928 at ([world] 284.8308269055609, 64.0, 247.39859903270533)
-2013-02-02 04:01:20 [INFO] [35;1m[Server] login's taking forever :/[m
-2013-02-02 04:01:34 [INFO] [[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> lol I need to add stripslashes...[m
-2013-02-02 04:01:43 [INFO] alanaktion issued server command: /gamemode 1
-2013-02-02 04:03:01 [INFO] alanaktion issued server command: /gm 0
-2013-02-02 04:29:28 [INFO] CONSOLE: [32;1mReload complete.[m
-2013-02-02 04:29:28 [WARNING] Can't keep up! Did the system time change, or is the server overloaded?
-2013-02-02 04:31:38 [INFO] send a message to a player[m
-2013-02-02 04:31:38 [INFO] /xmpp <player> <message>[m
-2013-02-02 04:31:43 [INFO] To view help from the console, type ?.[m
-2013-02-02 04:31:48 [INFO] [33;1m--------- [37;1mHelp: /xmpp [33;1m---------------------------[m
-2013-02-02 04:31:48 [INFO] [33;22mDescription: [37;1msend a message to a player[m
-2013-02-02 04:31:48 [INFO] [33;22mUsage: [37;1m/xmpp <player> <message>[m
+$log = <<<'EOT'
+33[0;32mhello world33[39m
 EOT;
+
+/*
+[[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> I got those skeletons good :D[m
+[[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> haha[m
+[[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> lol I have no idea what I'm doing :P[m
+[[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> hehe XD[m
+[[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> brb[m
+[35;1m[Server] login's taking forever :/[m
+[[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> lol I need to add stripslashes...[m
+CONSOLE: [32;1mReload complete.[m
+send a message to a player[m
+/xmpp <player> <message>[m
+To view help from the console, type ?.[m
+[33;1m--------- [37;1mHelp: /xmpp [33;1m---------------------------[m
+[33;22mDescription: [37;1msend a message to a player[m
+[33;22mUsage: [37;1m/xmpp <player> <message>[m
+*/
 
 $parsed = ansispan($log);
 
@@ -36,6 +32,7 @@ $parsed = ansispan($log);
 		pre {
 			padding: 5px;
 			border-radius: 4px;
+			color: #fff;
 			background: #000;
 			word-wrap: break-word;
 		}
