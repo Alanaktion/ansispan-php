f5c1acd2d75a38f802c65597d8cfbc1244d98964
Alan Hardman <me@imalan.tk>
2013-02-11 21:41:56 -0700

    Cleanup
    Commented-out code was removed

--- ansispan.inc.php
+++ ansispan.inc.php
@@ -60,22 +60,4 @@ function ansispan($str) {
 	
 }
 
-// mclogparse
-// Parses Minecraft log file and returns HTML
-/* OLD FUNCTION, HAD WAAAAAAY TOO MANY <SPAN> TAGS
-function mclogparse($str) {
-	$str = htmlspecialchars($str);
-	
-	$str = preg_replace("/\\[3([0-9a-f])\;(1m|22m|39m)/",'<span class="c${1}">',$str);
-	$str = strtr($str,array(
-		'[INFO]' => '[<span class="c7">INFO</span>]',
-		'[WARNING]' => '[<span class="c6">WARNING</span>]',
-		'[SEVERE]' => '[<span class="cc">SEVERE</span>]'
-	));
-	$str = str_replace('[0;39m','</span></span></span></span></span>',$str);
-	$str = str_replace('[m','</span></span></span></span></span></span></span></span></span></span>',$str);	// I have no idea what I'm doing with this :P
-	
-	return $str;
-}*/
-
 ?>
--- test.php
+++ test.php
@@ -5,23 +5,6 @@ $log = <<<'EOT'
 33[0;32mhello world33[39m
 EOT;
 
-/*
-[[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> I got those skeletons good :D[m
-[[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> haha[m
-[[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> lol I have no idea what I'm doing :P[m
-[[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> hehe XD[m
-[[34;1mSurvival[37;1m][0;39m<[31;22m~James[0;39m> brb[m
-[35;1m[Server] login's taking forever :/[m
-[[34;1mSurvival[37;1m][0;39m<[31;22m~Alan[0;39m> lol I need to add stripslashes...[m
-CONSOLE: [32;1mReload complete.[m
-send a message to a player[m
-/xmpp <player> <message>[m
-To view help from the console, type ?.[m
-[33;1m--------- [37;1mHelp: /xmpp [33;1m---------------------------[m
-[33;22mDescription: [37;1msend a message to a player[m
-[33;22mUsage: [37;1m/xmpp <player> <message>[m
-*/
-
 $parsed = ansispan($log);
 
 ?><!DOCTYPE html>
